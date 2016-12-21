aeec391ab0195637487c5f125ce74dcaa866fc7b
Sam Rudge <sam.rudge@songkick.com>
2016-12-21 13:00:17 +0000

    Use instance metadata API to get instance ID if it isn't provided

--- main.go
+++ main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/YoSmudge/server-names/namer"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
+const instanceIdUrl string = "http://169.254.169.254/latest/meta-data/instance-id"
+
 func main() {
-	serverId := os.Args[1]
+	var serverId string
+
+	if len(os.Args) >= 2 {
+		serverId = os.Args[1]
+	}
+
+	if serverId == "" {
+		var ra []byte
+		r, err := http.Get(instanceIdUrl)
+
+		if err == nil {
+			ra, err = ioutil.ReadAll(r.Body)
+		}
+
+		if err != nil {
+			fmt.Println("Instance ID not supplied and could not talk to the instance metadata service:", err)
+			os.Exit(1)
+		}
+		serverId = string(ra)
+	}
 
 	name, err := namer.Name(serverId)
 	if err != nil {
